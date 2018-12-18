45d047d8287e2a609b60e36933bff26ff24481ca
tianwenbin <972619117@qq.com>
2018-12-18 16:58:49 +0800

    提交代码

---
+++ src/components/Luck.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container">
+    <el-row>
+      <el-col :span="8"><div class="people_box"><ul>
+        <li class="people_box_item" v-for="d1 in peopleList" :key="d1.id">
+          <div class="people_box_tt">{{d1.name}}</div>
+          <div class="people_box_ct" v-if="d1.children && d1.children.length"><ul>
+            <li class="people_name" v-for="d2 in d1.children" :key="d2.id">
+              <el-button @click="nameClickEvn(d2)" :type="d2 | filterButtonType" size="normal">{{d2.name}}</el-button>
+            </li>
+          </ul></div>
+        </li>
+        <li class="people_box_item">
+          <div class="people_box_ct people_box_all"><ul>
+            <li class="people_name">总共 <span class="people_name--all">{{allLength}}</span> 人，已选 <span class="people_name--all">{{activeLength}}</span> 人</li>
+          </ul></div>
+        </li>
+      </ul></div></el-col>
+      <el-col :span="16"><div class="grid-content bg-purple-light">s</div></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script type="text/babel">
+  export default {
+    name: 'Luck',
+    data () {
+      return {
+        //typeVal:1:正常  2:选中 3:不可用
+        //type 1:代表一级（头部）  2:代表二级（人信息）
+        peopleList: [
+          {id:"1",name:"设计",type:"1",typeVal:"1",children:[
+            {id:"1_1",name:"张某某",type:"2",typeVal:"1"},{id:"1_2",name:"陈某某",type:"2",typeVal:"1"},
+            {id:"1_1",name:"张某某",type:"2",typeVal:"3"},{id:"1_2",name:"陈某某",type:"2",typeVal:"3"},
+          ]},
+          {id:"2",name:"前端",type:"1",typeVal:"1",children:[
+            {id:"2_1",name:"黄某某",type:"2",typeVal:"1"},{id:"2_2",name:"彭某某",type:"2",typeVal:"1"}
+          ]},
+        ],
+        allLength:0,//共多少人
+        activeLength:0,//选中的多少人
+      }
+    },
+    filters:{
+      filterButtonType(d){
+        let str = "";
+        switch (d.typeVal){
+          case "3":
+            str="info";
+            break;
+          default:
+            str="default";
+        }
+        return str;
+      }
+    },
+    methods:{
+      nameClickEvn(d){
+        switch (d.typeVal){
+          case "3":
+            d.typeVal="1";
+            break;
+          default:
+            d.typeVal="3";
+        }
+        this.dealPeopleNum();
+      },
+      dealPeopleNum(){
+        let allL=0;
+        let activeL=0;
+        for(let i=0,l=this.peopleList.length; i<l; i++){
+          let d1=this.peopleList[i].children;
+          if(!d1 || !d1.length) continue;
+          for(let j=0,k=d1.length; j<k; j++){
+            let d2 = d1[j];
+            if(d2.typeVal=="1"){//选中的加加
+              activeL++;
+            }
+            allL++;//总数加一
+          }
+        }
+      this.allLength = allL;
+      this.activeLength = activeL;
+      }
+    },
+    mounted(){
+      this.dealPeopleNum();
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .people_box{padding: 0 20px;}
+  .people_box_item{border: 1px solid #ddd;}
+  .people_box_item+.people_box_item{border-top: none;}
+  .people_box_tt{padding-left: 5px; text-align: left; line-height: 30px; border-bottom: 1px solid #ddd; font-weight: bold;}
+  .people_box_ct{overflow: hidden;}
+  .people_name{float: left; padding: 5px;}
+  .people_name--all{font-size: 18px; font-weight: bold; color: #3a8ee6;}
+  .people_box_all{border-top: 2px solid #ddd;}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/reset.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
